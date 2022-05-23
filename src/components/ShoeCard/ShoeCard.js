a04a2a72c6f332ea16e9dbf5203e6fa6f5d39e78
Michael Jang <michaeljang86@gmail.com>
2022-05-23 01:40:00 -0400

    ShoeCards are finished

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -31,19 +31,38 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  const styles = {
+    "on-sale": {
+      "--priceColor": COLORS.gray[700],
+      "--textDecoration": "line-through",
+      "--flagColor": COLORS.primary,
+    },
+    "new-release": {
+      "--flagColor": COLORS.secondary,
+    },
+  };
+
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant !== "default" ? (
+            <Flag style={styles[variant]}>
+              {variant === "on-sale" ? "Sale" : "Just Released"}
+            </Flag>
+          ) : null}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price style={styles[variant]}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" ? (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          ) : null}
         </Row>
       </Wrapper>
     </Link>
@@ -63,10 +82,13 @@ const ImageWrapper = styled.div`
 
 const Image = styled.img`
   width: 100%;
+  border-radius: 16px 16px 4px 4px;
 `;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -74,7 +96,11 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: var(--priceColor);
+  text-decoration: var(--textDecoration);
+  text-decoration-thickness: 10%;
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -83,6 +109,19 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+`;
+
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  height: 32px;
+  line-height: 32px;
+  padding: 0px 10px;
+  border-radius: 2px;
+  background-color: var(--flagColor);
+  color: white;
+  font-weight: 700;
 `;
 
 export default ShoeCard;
